223c9e5089a135518272a194e86df5d8779f302e
Nivetha-I <nivethailangoselvan@gmail.com>
2024-01-05 11:33:33 +0530

    first commit

---
+++ airbnb.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+from streamlit_option_menu import option_menu
+pd.set_option('display.max_columns', None)
+import plotly.express as px
+import warnings
+warnings.filterwarnings("ignore")
+import plotly.graph_objects as go
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+
