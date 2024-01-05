cc67674029ff40ebb9889f643b953f83a79528e7
Nivetha-I <nivethailangoselvan@gmail.com>
2024-01-05 15:55:49 +0530

    second commit

--- airbnb.py
+++ airbnb.py
@@ -10,3 +10,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from PIL import Image
 
+# Streamlit part
+
+st.set_page_config(layout= "wide")
+st.title(" :red[AIRBNB DATA ANALYSIS]")
+st.write("")
+
+
+def datafr():
+    df= pd.read_csv("C:/Users/admin/Desktop/Airbnb_Analysis_project/Airbnb_analysis.csv")
+    return df
+
+df= datafr()
+
+with st.sidebar:
+    select= option_menu("Main Menu", ["Home", "Data Exploration", "About"])
+    logoimage = Image.open("C:/Users/admin/Desktop/Airbnb_Analysis_project/airbnblogo_new.jpg")
+    st.image(logoimage)
+
+if select == "Home":
+
+    image1= Image.open("C:/Users/admin/Desktop/Airbnb_Analysis_project/airbnblogo_with_house.jpg")
+    st.image(image1)
+    st.write("")
+    st.write('''***Airbnb, Inc. is an American San Francisco-based company operating an online marketplace for short- and long-term homestays and experiences.
+                The company acts as a broker and charges a commission from each booking.
+                The company was founded in 2008 by Brian Chesky, Nathan Blecharczyk, and Joe Gebbia. Airbnb is a shortened version of its original name, AirBedandBreakfast.com.
+                Airbnb is the most well-known company for short-term housing rentals.
+                  Unique stays, Experiences, Adventures, and more.
+                  24/7 support***''')
+    
